a9a6cb1895c6ffe0840815547d83e007cc08c4bb
leloeduk <leloeduk2025@gmail.com>
2025-07-28 19:04:03 +0200

    Fix DATABASE_URL configuration

--- lelobook/settings.py
+++ lelobook/settings.py
@@ -107,15 +107,15 @@ CSRF_COOKIE_SECURE = True
 
 
 
+# Configuration avec valeur par défaut (SQLite en développement)
 DATABASES = {
-    'default': dj_database_url.parse(
-        config('DATABASE_URL'),  # Doit être défini dans les variables d'environnement
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600,
         conn_health_checks=True,
-        ssl_require=True  # Important pour les connexions externes sécurisées
+        ssl_require=True  # Important pour les BDD externes
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
